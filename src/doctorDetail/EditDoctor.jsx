136fca39884fbe1ef3d7e47294865fb132059a21
guideeeeeee <pratom5108@gmail.com>
2023-11-03 17:22:06 +0700

    90% no responsive

--- src/doctorDetail/EditDoctor.jsx
+++ src/doctorDetail/EditDoctor.jsx
@@ -1,34 +1,118 @@
 import "./EditDoctor.css";
+import Profile from "./component/profile.svg";
 function EditDoctor() {
   return (
     <>
       <div className="wpage">
-        
         <div className="topForm">
           {/* topform */}
           <div className="leftForm">
-
             {/* fullname */}
             <div className="fullname">
-              <div className="firstname">
-                <text className="fullname-text">Firstname:</text>
+              <div className="fullname-text">
+                <label className="fntext">First Name :</label>
                 <input type="text"></input>
               </div>
-              <div className="lastname">
-                <text className="fullname-text">Lastname:</text>
+              <div className="fullname-text">
+                <label className="fntext">Last Name :</label>
                 <input type="text"></input>
               </div>
             </div>
-
             {/* specialized */}
             <div className="expertise">
-                <text className="normalText">Specialized expertise:</text>
-                <input type="text"></input>
+              <text className="normalText">Specialized expertise :</text>
+              <input type="text"></input>
+            </div>
+            {/* PLAB */}
+            <div className="Plab">
+              <text className="normalText">PLAB :</text>
+              <input type="text"></input>
             </div>
           </div>
-          <div className="RightForm"></div>
         </div>
-        <div className="bottomForm">{/* bottomform */}</div>
+        <div className="bottomForm">
+          <div className="part1">
+            <div className="test">
+              <text className="normalText">Centers and Clinics :</text>
+              <input type="text" className="centers"></input>
+              <text className="normalText">Language:</text>
+              <input type="text" className="language"></input>
+            </div>
+            <div className="test2">
+              <text className="normalText">Treatment and service :</text>
+              <input type="text" className="treatment"></input>
+            </div>
+          </div>
+          <div className="part2">
+            <div className="part2-1">
+              <text className="normalText">Graduated education :</text>
+            </div>
+            <div className="theBox">
+              <div className="year">
+                <text className="normalText">Year :</text>
+                <input type="text" className="year-input"></input>
+                <input type="text" className="year-input"></input>
+                <input type="text" className="year-input"></input>
+              </div>
+              <div className="S-expertise">
+                <text className="normalText">Specialized expertise :</text>
+                <input type="text" className="spec-input"></input>
+                <input type="text" className="spec-input"></input>
+                <input type="text" className="spec-input"></input>
+              </div>
+              <div className="S-expertise">
+                <text className="normalText">University :</text>
+                <input type="text" className="spec-input"></input>
+                <input type="text" className="spec-input"></input>
+                <input type="text" className="spec-input"></input>
+              </div>
+            </div>
+            <div className="part3">
+              <text className="normalText">Work experience :</text>
+              <div className="theBox-1">
+                <div className="duration">
+                  <text className="normalText">Duration :</text>
+                  <input type="text" className="du-input"></input>
+                  <input type="text" className="du-input"></input>
+                  <input type="text" className="du-input"></input>
+                  <input type="text" className="du-input"></input>
+                  <input type="text" className="du-input"></input>
+                </div>
+                <div className="S-expertise">
+                  <text className="normalText">Specialized expertise :</text>
+                  <input type="text" className="spec-input"></input>
+                  <input type="text" className="spec-input"></input>
+                  <input type="text" className="spec-input"></input>
+                  <input type="text" className="spec-input"></input>
+                  <input type="text" className="spec-input"></input>
+                </div>
+                <div className="wtfcol">
+                  <input type="text" className="wtf-input"></input>
+                  <input type="text" className="wtf-input"></input>
+                </div>
+              </div>
+            </div>
+            <div className="part4">
+              <div className="theBox-1">
+                <div className="gender">
+                  <text className="normalText">Gender :</text>
+                  <input type="text" className="du-input"></input>
+                </div>
+                <div className="tel">
+                  <text className="normalText">Tel :</text>
+                  <input type="number" className="tel-input"></input>
+                </div>
+                <div className="tel">
+                  <text className="normalText">ID Line :</text>
+                  <input type="text" className="tel-input"></input>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="finalpart">
+            <button className="btnfinal">CONFIRM</button>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import EditDoctor from './doctorDetail/EditDoctor'
+import Navbar from './navbar/navbar'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
+    <Navbar/>
     <EditDoctor/>
     </>
   </React.StrictMode>
